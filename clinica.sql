bc0dff7aaf15c84573ec985c4e88599967bfbcf9
Dayrons <dayronstovar@gmail.com>
2022-02-14 18:08:53 -0400

    recursos necesarios

--- clinica.sql
+++ clinica.sql
@@ -1,110 +1,48 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Linux (x86_64)
---
--- Host: 127.0.0.1    Database: clinica
--- ------------------------------------------------------
--- Server version	8.0.28-0ubuntu0.20.04.3
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ clinica /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE clinica;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `citas`
---
-
-DROP TABLE IF EXISTS `citas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+DROP TABLE IF EXISTS citas;
 CREATE TABLE `citas` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
-  `paciente` int NOT NULL,
-  `doctor` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `paciente` int(11) NOT NULL,
+  `doctor` int(11) NOT NULL,
   `sintomas` text NOT NULL,
+  `status` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `citas`
---
-
-/*!40000 ALTER TABLE `citas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `citas` ENABLE KEYS */;
-
---
--- Table structure for table `doctores`
---
-
-DROP TABLE IF EXISTS `doctores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+DROP TABLE IF EXISTS doctores;
 CREATE TABLE `doctores` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
   `nombre` varchar(20) NOT NULL,
   `apellido` varchar(20) NOT NULL,
   `especializacion` varchar(100) NOT NULL,
   `genero` varchar(1) NOT NULL,
-  `edad` int NOT NULL,
+  `edad` int(11) NOT NULL,
   `telefono` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `password` text NOT NULL,
   `dni` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `dni` (`dni`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `doctores`
---
-
-/*!40000 ALTER TABLE `doctores` DISABLE KEYS */;
-INSERT INTO `doctores` VALUES (2,'cristian','tovar','dermatologia','M',21,'04120148704','cristian@example.com','13727194');
-/*!40000 ALTER TABLE `doctores` ENABLE KEYS */;
-
---
--- Table structure for table `pacientes`
---
-
-DROP TABLE IF EXISTS `pacientes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+DROP TABLE IF EXISTS pacientes;
 CREATE TABLE `pacientes` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
   `nombre` varchar(50) DEFAULT NULL,
   `apellido` varchar(50) DEFAULT NULL,
   `genero` varchar(1) DEFAULT NULL,
-  `edad` int DEFAULT NULL,
+  `edad` int(11) DEFAULT NULL,
   `telefono` varchar(50) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `dni` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `dni` (`dni`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `pacientes`
---
-
-/*!40000 ALTER TABLE `pacientes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pacientes` ENABLE KEYS */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-02-13 21:58:03
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
